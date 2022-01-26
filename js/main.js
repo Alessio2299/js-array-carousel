0dadcee3de898195b10f8812ca70a5f77defb061
Alessio Napoli <alessionapoli99@icloud.com>
2022-01-26 16:10:51 +0100

    Created cycle "For"

---
+++ js/main.js
@@ -0,0 +1,47 @@
+let containerImg = document.querySelector(".box-principal-img");
+let titleImg = document.getElementById("title");
+let textImg = document.getElementById("text-title");
+let asideImg = document.querySelector(".box-aside-img")
+
+const img = [
+  'img/01.jpg',
+  'img/02.jpg',
+  'img/03.jpg',
+  'img/04.jpg',
+  'img/05.jpg'
+];
+const title = [
+  'Svezia',
+  'Svizzera',
+  'Gran Bretagna',
+  'Germania',
+  'Paradise'
+]
+const text = [
+  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+  'Lorem ipsum',
+  'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+  'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+]
+let slides = "";
+let contentTitle = "";
+let contentText = "";
+let aside = "";
+
+for (let i=0; i < img.length; i++){
+
+  slides +=`<img src="${img[i]}">`;
+
+  contentTitle += `<h2>${title[i]}</h2>`;
+
+  contentText += `<p>${text[i]}</p>`;
+  
+  aside += `<div class="aside-img">
+              <img src="${img[i]}">
+            </div>`;
+}
+containerImg.innerHTML = slides;
+titleImg.innerHTML = contentTitle;
+textImg.innerHTML = contentText;
+asideImg.innerHTML += aside;
